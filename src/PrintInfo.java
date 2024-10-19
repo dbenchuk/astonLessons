2d76a4215389b899560358808cbfc12e1ea4b12b
Dmitry Benchuk <dbenchuk@gmail.com>
2024-10-19 18:25:39 +0300

    c

---
+++ src/PrintInfo.java
@@ -0,0 +1,9 @@
+public class PrintInfo {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords(){
+        System.out.println("Orange\nBanana\nApple");
+    }
+}
